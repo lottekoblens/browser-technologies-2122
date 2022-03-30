0e5f83490d0c5741c63d75b10f68a32e10ccf811
Lotte Koblens <lotte.koblens@hva.nl>
2022-03-30 15:11:24 +0200

    pages subjects made

--- index.js
+++ index.js
@@ -17,6 +17,54 @@ app.get('/css', (req, res) => {
     res.render('css')
 })
 
+app.post('/wafs', (req, res) => {
+    res.render('wafs')
+})
+
+app.get('/wafs', (req, res) => {
+    res.render('wafs')
+})
+
+app.post('/bt', (req, res) => {
+    res.render('bt')
+})
+
+app.get('/bt', (req, res) => {
+    res.render('bt')
+})
+
+app.post('/pwa', (req, res) => {
+    res.render('pwa')
+})
+
+app.get('/pwa', (req, res) => {
+    res.render('pwa')
+})
+
+app.post('/hcd', (req, res) => {
+    res.render('hcd')
+})
+
+app.get('/hcd', (req, res) => {
+    res.render('hcd')
+})
+
+app.post('/rtw', (req, res) => {
+    res.render('rtw')
+})
+
+app.get('/rtw', (req, res) => {
+    res.render('rtw')
+})
+
+app.post('/end', (req, res) => {
+    res.render('end')
+})
+
+app.get('/end', (req, res) => {
+    res.render('end')
+})
+
 app.use((req, res) => {
     res.status(404).send('Sorry, deze pagina kon ik niet vinden.');
 });
