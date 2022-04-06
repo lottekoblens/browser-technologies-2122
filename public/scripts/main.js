03068b112b19273a3e836a333decf1db54a795eb
Lotte Koblens <lotte.koblens@hva.nl>
2022-04-06 13:33:34 +0200

    fallback for when required doesn't work added

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -8,3 +8,19 @@ if (changeAnswersSelect) {
         changeAnswersButton.setAttribute('href', changeAnswersSelect.value);
     });
 }
+
+// when required isn't supported 
+const input = document.querySelectorAll('input')
+const form = document.querySelector('form')
+
+const emptyInput = () => {
+    input.forEach(input => {
+        if (input.value.length == 0) {
+            form.addEventListener('submit', () => {
+                alert('Je hebt nog niet alles ingevuld')
+            })
+        }
+    })
+}
+
+emptyInput();
