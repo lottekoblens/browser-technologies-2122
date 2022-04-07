2dbd55bab2f25c6b4a5c2c55f366c8508c550dd9
Lotte Koblens <lotte.koblens@hva.nl>
2022-04-07 09:58:22 +0200

    fixed js form submit

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,38 +1,40 @@
-const changeAnswersSelect = document.querySelector('select');
+// if the user wants to change a answer, this brings them to the page they chose to change the answers of
+const AnswersSelect = document.querySelector('select');
 const changeAnswersButton = document.querySelector('#change_answers_button');
 
-if (changeAnswersSelect) {
-    changeAnswersButton.setAttribute('href', changeAnswersSelect.value);
+if (AnswersSelect) {
+    changeAnswersButton.setAttribute('href', AnswersSelect.value);
 
-    changeAnswersSelect.addEventListener('change', () => {
-        changeAnswersButton.setAttribute('href', changeAnswersSelect.value);
+    AnswersSelect.addEventListener('change', () => {
+        changeAnswersButton.setAttribute('href', AnswersSelect.value);
     });
 }
 
-// when required isn't supported 
+// when required isn't supported this function will prevent the user from submitting the form without filling in all
+// the input fields and textareas
 const inputs = document.querySelectorAll('input')
 const textareas = document.querySelectorAll('textarea')
 const form = document.querySelector('form')
 
 const checkInputs = () => {
-    inputs.forEach(input => {
-        if (input.value.length == 0) {
-            form.addEventListener('submit', (event) => {
+    form.addEventListener('submit', (event) => {
+        inputs.forEach(input => {
+            if (input.value.length == 0) {
+                event.preventDefault()
                 alert('Nog niet alle velden zijn ingevuld');
-            })
-        }
-    });
-    textareas.forEach(textarea => {
-        if (textarea.value.length == 0) {
-            form.addEventListener('submit', () => {
+            }
+        })
+        textareas.forEach(textarea => {
+            if (textarea.value.length == 0) {
+                event.preventDefault()
                 alert('Nog niet alle velden zijn ingevuld');
-            })
-        }
-    });
+            }
+        });
+    })
 }
 checkInputs();
 
-
+// add class loading on page load
 window.onload = () => {
     document.body.classList.add('loading');
 }
