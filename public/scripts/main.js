4cc7a2fa7db15d2d94b2a229096b8cd6ed9c3a95
Lotte Koblens <lotte.koblens@hva.nl>
2022-04-07 11:51:22 +0200

    es6 > es5

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,57 +1,59 @@
 // if the user wants to change a answer, this brings them to the page they chose to change the answers of
-const AnswersSelect = document.querySelector('select');
-const changeAnswersButton = document.querySelector('#change_answers_button');
+var AnswersSelect = document.querySelector('select');
+var changeAnswersButton = document.querySelector('#change_answers_button');
 
 // check if classlist is supported or not
-if (document.body.classList) {
-    AnswersSelect.classList.add('show'); //only show the select when javascript works
-} else {
-    AnswersSelect.style.display = 'block';
-}
+
 
 if (AnswersSelect) {
     changeAnswersButton.setAttribute('href', AnswersSelect.value);
 
-    AnswersSelect.addEventListener('change', () => {
+    AnswersSelect.addEventListener('change', function () {
         changeAnswersButton.setAttribute('href', AnswersSelect.value);
     });
+
+    if (document.body.classList) {
+        AnswersSelect.classList.add('show'); //only show the select when javascript works
+    } else {
+        AnswersSelect.style.display = 'block';
+    }
 }
 
 // when required isn't supported this function will prevent the user from submitting the form without filling in all
 // the input fields and textareas
-const inputs = document.querySelectorAll('input')
-const textareas = document.querySelectorAll('textarea')
-const form = document.querySelector('form')
+var inputs = document.querySelectorAll('input')
+var textareas = document.querySelectorAll('textarea')
+var form = document.querySelector('form')
 
-const checkInputs = () => {
-    form.addEventListener('submit', (event) => {
-        inputs.forEach(input => {
-            if (input.value.length == 0) {
+function checkInputs() {
+    form.addEventListener('submit', function (event) {
+        for (var i = 0; i < inputs.length; i++) {
+            if (inputs[i].value.length == 0) {
                 event.preventDefault()
                 alert('Nog niet alle velden zijn ingevuld');
             }
-        })
-        textareas.forEach(textarea => {
-            if (textarea.value.length == 0) {
+        }
+        for (var i = 0; i < textareas.length; i++) {
+            if (textareas[i].value.length == 0) {
                 event.preventDefault()
                 alert('Nog niet alle velden zijn ingevuld');
             }
-        });
+        }
     })
 }
-if (document.body.forEach) {
-    checkInputs();
-}
+
+checkInputs();
+
 
 // add class loading on page load
 
 // check if classlist is supported or not
 if (document.body.classList) {
-    window.onload = () => {
+    window.onload = function () {
         document.body.classList.add('loading');
     }
 } else {
-    window.onload = () => {
+    window.onload = function () {
         document.body.style.animation = 'fadein 1s'
     }
 }
